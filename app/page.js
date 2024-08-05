81802a5646dfd3e7a685c82af03c3a774b604b95
poojakedia <poojakedianov@gmail.com>
2024-08-04 22:29:07 -0400

    initial app structure

--- app/page.js
+++ app/page.js
@@ -1,95 +1,146 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import {useState, useEffect} from 'react'
+import { Box,Stack, Typography, Button, Modal, TextField} from '@mui/material'
+import { firestore } from '@/firebase'
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  deleteDoc,
+  getDoc,
+  setDoc,
+  updateDoc,
+} from 'firebase/firestore'
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column', 
+  gap: 3
 }
+
+export default function Home(){
+  const [open, setOpen] = useState(false)
+  const [clothing, setClothing] = useState('')
+  const [closet, setCloset] = useState([])
+
+  const updateCloset = async () =>{
+    const snapshot = query(collection(firestore, 'closet'))
+    const docs = await getDocs(snapshot)
+    const closetList = []
+    docs.forEach(doc => {
+      closetList.push({clothing: doc.id, ...doc.data()})
+    })
+    setCloset(closetList)
+  }
+    useEffect(()=>{
+      updateCloset()
+    },[])
+
+  
+  /* basically queries the closet collection in firestore and updates the local state
+  which is our registry of items, the useEffect hook updates the closet inventory whenever the component
+  mounts, meaning this block will run everytime the closet inventory is updated
+  */
+
+  const addItem = async(item) => {
+    const docRef = doc(collection(firestore, 'closet'), item)
+    const docSnap = await getDoc(docRef)
+    if(docSnap.exists()){
+      const { quantity } = docSnap.data()
+      if (quantity === 1){
+        await deleteDoc(docRef)
+      } else {
+        await updateDoc(docRef, { quantity: quantity - 1 })
+      }
+    }
+        await updateCloset(); // after creating update your local state by fetching the new dat from Firestore
+  }
+  const removeItem = async(item) => {
+    const docRef = doc(collection(firestore, 'closet'), item)
+    const docSnap = await getDoc(docRef)
+    if(docSnap.exists()){
+      const { quantity } = docSnap.data()
+      if(quantity === 1) {
+        await deleteDoc(docRef)// deletes the item from the closet if the quantity is 1
+      } else{
+        await setDoc(docRef, { quantity: quantity - 1})// decrements the item by 1 if there are multiple
+      }
+    }
+      await updateCloset();
+    }
+
+  /*manage the modal state- determining whether a component is visible or hidden*/
+  const handleOpen = () => {
+    setOpen(true)
+    }
+  const handleClose = () =>{
+    setOpen(false)
+  }
+
+  return(
+    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center',
+      height: '100vh', gap: 2, height: '100vw', width: '100wv'}}>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Add Item
+            </Typography>
+            <Stack width = '100%' direction = {'row'} spacing = {2}>
+            <TextField id="outlined-basic" label = "Item" variant = "outlined" fullWidth value = {clothing} onChange ={(e) => setClothing(e.target.value)}
+            />
+            <Button variant = 'outlined' 
+            onClick = {() => {addItem(clothing)
+              setClothing('')
+              handleClose()
+            }}>
+              Add
+            </Button>
+            </Stack>
+          </Box>
+
+        </Modal>
+        <Button variant = 'contained' onClick = {handleOpen}>Add Item</Button>
+        <Box border = {'1px solid #333'}>
+          <Box
+          width='800px'
+          height='100px'
+          bgcolor = {'#ADD8E6'}
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          >
+            <Typography variant = 'h2' color = {'#333'} textAlign={'center'}>
+              Your Closet
+            </Typography>
+          </Box>
+          <Stack width='800px' height='300px' spacing={2} overflow={'auto'}>
+            {closet.map((name, quantity) => (
+              <Box key = {name} display={'flex'} justifyContent={'space-between'} alignItems={'center'} bgcolor={'#f0f0f0'} paddingX={5}>
+                <Typography variant = 'h3' color='#333' textAlign = {'center'}>{name.charAt(0).toUpperCase()+ name.slice(1)}</Typography>
+                <Typography variant = 'h3' color='#333' textAlign = {'center'}>Quantity: {quantity}</Typography>
+                <Button variant = 'contained' onClick = {() => {removeItem(name)}}>Remove
+                  </Button>
+              </Box>
+  ))}
+          </Stack>
+        </Box>
+      <Typography variant="h2">Clueless Closet</Typography>
+      </Box>
+  )
+  }
